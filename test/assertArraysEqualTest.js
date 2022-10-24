a6e39cb1fabc6918d88acb63878957af76576b35
WesleyMatthee <wrmatthee@gmail.com>
2022-10-24 20:02:57 +0000

    pushing more test files for assertArraysEqualsTest

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+
+
+//test code
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 4, 3, 5], [1, 4, 3]);
+assertArraysEqual([4, 2, 3], [4, 2, 3]);
+assertArraysEqual([3, 2, 3], [3, 2, 3]); 
